34d171fe4945e655669a56c400efd608cd602b5e
fjc <1134118878@qq.com>
2019-10-29 20:35:52 +0800

    管理模块整合

--- gmall-oms/src/main/java/com/atguigu/gmall/oms/GmallOmsApplication.java
+++ gmall-oms/src/main/java/com/atguigu/gmall/oms/GmallOmsApplication.java
@@ -1,9 +1,15 @@
 package com.atguigu.gmall.oms;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableSwagger2
+@MapperScan(basePackages = "com.atguigu.gmall.oms.dao")
 public class GmallOmsApplication {
 
     public static void main(String[] args) {
---
+++ gmall-wms/src/main/java/com/atguigu/gmall/wms/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.atguigu.gmall.wms.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by FJC on 2019-10-29.
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/**").permitAll();
+        super.configure(http);
+    }
+}
